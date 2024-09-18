f4750754c1781b18336fc69315093c428911f2a1
Roosa <roosa.rintamaki@tuni.fi>
2024-09-18 10:47:16 +0300

    Removed  test accurary function and replaced it with test data function

--- mnistaccuracy.py
+++ mnistaccuracy.py
@@ -3,24 +3,21 @@ import tensorflow as tf
 from sklearn.neighbors import KNeighborsClassifier
 
 
-
 def class_acc(pred,gt):
+
     pred = np.array(pred)
     gt = np.array(gt)
-
     correctpreds = np.sum(pred == gt)
     totalpreds = len(gt)
-
     accuracy = correctpreds / totalpreds
 
     return accuracy
 
-def test_accuracy():
-    num_test_samples = len(y_test)
-    random_predictions = np.random.randint(0, 10, num_test_samples)
+def test_data(knn, x_test, y_test):
+    y_pred = knn.predict(x_test)
+    accuracy = class_acc(y_pred, y_test)  
+    print(f"Classification accuracy is {accuracy:.2f}")
 
-    accuracy = class_acc(random_predictions, y_test)
-    print(f"Accuracy of random predictions on MNIST test set: {accuracy:.2f}")
 
 def preprocess_data(x_train, x_test):
     # Flatten the images and normalize pixel values to the range [0, 1]
@@ -29,12 +26,14 @@ def preprocess_data(x_train, x_test):
 
     return x_train_flattened, x_test_flattened
 
-def train_data(x_train_flattened, y_train):
+def train_data(x_train, y_train):
     # Create a KNN classifier
     knn = KNeighborsClassifier(n_neighbors=5)  # You can change n_neighbors to whatever you like
 
     # Train the classifier
-    knn.fit(x_train_flattened, y_train)
+    knn.fit(x_train, y_train)
+
+    return knn
 
 
 def main():
@@ -42,7 +41,7 @@ def main():
     mnist = tf.keras.datasets.mnist
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-    x_train_flattened, x_test_flattened = preprocess_data(x_train, y_train)
+    x_train_flattened, x_test_flattened = preprocess_data(x_train, x_test)
 
     train_data(x_train_flattened, y_train)
 
