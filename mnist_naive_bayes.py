bef9667bd12427034bb58811a9b33134e7a2cd87
Roosa <roosa.rintamaki@tuni.fi>
2024-09-18 14:00:35 +0300

    Added noise to training data

--- mnist_naive_bayes.py
+++ mnist_naive_bayes.py
@@ -21,6 +21,13 @@ def preprocess_data(x_train, x_test):
 
     return x_train_flattened, x_test_flattened
 
+def add_noise(x_train):
+    noise = np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
+    x_train_noise = x_train + noise
+    
+    return np.clip(x_train_noise, 0.0, 1.0)
+
+
 def train_data(x_train, y_train):
     # Create a KNN classifier
     knn = KNeighborsClassifier(n_neighbors=5)  
@@ -58,6 +65,31 @@ def calculate_variances(x_train, classes):
 
     return np.array(variance)
 
+def log_likelihood(x, mean, var):
+    # Calculates logaritmic likehood for every test sample
+    epsilon=1e-8
+    var = var + epsilon
+
+    term1 = -0.5 * np.log(2 * np.pi) # Constant for every class
+    term2 = -0.5 * np.log(var)
+    term3 = -0.5 * ((x-mean)**2)/var
+
+    log_likelihood = np.sum(term1 + term2 + term3)
+    return log_likelihood
+
+
+def predict_class(x, means, variances):
+    # x is the test sample
+    # means and variances are the lists of mean and variance vectors for each class
+    num_classes = len(means)
+    log_likelihoods = []
+    
+    for i in range(num_classes):
+        log_likelihood_k = log_likelihood(x, means[i], variances[i])
+        log_likelihoods.append(log_likelihood_k)
+    
+    # Return the class with the highest log likelihood
+    return np.argmax(log_likelihoods)
 
 
 def main():
@@ -67,12 +99,16 @@ def main():
 
     x_train_flattened, x_test_flattened = preprocess_data(x_train, x_test)
 
-    knn = train_data(x_train_flattened, y_train)
+    x_train_noise = add_noise(x_train_flattened)
+
+    knn = train_data(x_train_noise, y_train)
 
     test_data(knn, x_test_flattened, y_test)
 
-    means = calculate_means(x_train_flattened, y_train)
-    variances = calculate_variances(x_train_flattened, y_train)
+    means = calculate_means(x_train_noise, y_train)
+    variances = calculate_variances(x_train_noise, y_train)
+
+    predict_class(x_test_flattened, means, variances)
 
 if __name__ == "__main__":
     main()
