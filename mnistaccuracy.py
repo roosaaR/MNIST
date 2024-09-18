f68f10d994f6ad89658cc9ab6f2a9f572c142f93
Roosa <roosa.rintamaki@tuni.fi>
2024-09-18 10:35:37 +0300

    Created functions and added rf_mnist_illustrate.py file to directory

--- mnistaccuracy.py
+++ mnistaccuracy.py
@@ -15,27 +15,36 @@ def class_acc(pred,gt):
 
     return accuracy
 
+def test_accuracy():
+    num_test_samples = len(y_test)
+    random_predictions = np.random.randint(0, 10, num_test_samples)
 
-# Load MNIST data
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+    accuracy = class_acc(random_predictions, y_test)
+    print(f"Accuracy of random predictions on MNIST test set: {accuracy:.2f}")
 
-# Step 2: Generate random predictions
-num_test_samples = len(y_test)
-random_predictions = np.random.randint(0, 10, num_test_samples)
+def preprocess_data(x_train, x_test):
+    # Flatten the images and normalize pixel values to the range [0, 1]
+    x_train_flattened = x_train.reshape(x_train.shape[0], -1) / 255.0
+    x_test_flattened = x_test.reshape(x_test.shape[0], -1) / 255.0
 
-# Step 3: Compute accuracy
-accuracy = class_acc(random_predictions, y_test)
-print(f"Accuracy of random predictions on MNIST test set: {accuracy:.2f}")
+    return x_train_flattened, x_test_flattened
+
+def train_data(x_train_flattened, y_train):
+    # Create a KNN classifier
+    knn = KNeighborsClassifier(n_neighbors=5)  # You can change n_neighbors to whatever you like
+
+    # Train the classifier
+    knn.fit(x_train_flattened, y_train)
 
 
-# Flatten the images and normalize pixel values to the range [0, 1]
-x_train_vector = x_train.reshape(x_train.shape[0], -1) / 255.0
-x_test_vector = x_test.reshape(x_test.shape[0], -1) / 255.0
+def main():
+    # Load the MNIST data
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# Create a KNN classifier
-knn = KNeighborsClassifier(n_neighbors=5)  # You can change n_neighbors to whatever you like
+    x_train_flattened, x_test_flattened = preprocess_data(x_train, y_train)
 
-# Train the classifier
-knn.fit(x_train_vector, y_train)
+    train_data(x_train_flattened, y_train)
 
+if __name__ == "__main__":
+    main()
